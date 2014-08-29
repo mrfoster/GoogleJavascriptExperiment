ba60eaef77dd39e4d0ebafc5efacc8685150e4d6
mrfoster <christopherfoster@hotmail.co.uk>
2014-08-29 06:35:09 +0100

    Update settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -5,8 +5,8 @@ function define(name, value) {
     });
 }
 
-define("googleApiKey", "AIzaSyDMfAThHSvNcRPRJhCu9H-Rjls55qXVAA0");
-define("googleCustomSearchKey", "003998223922167696135:q_zrl4_xvbw");
+define("googleApiKey", "Xxxxxxxxx");
+define("googleCustomSearchKey", "xxxxxxxxx");
 define("message", "When will Google crawl JavaScript?");
 define("sendGridUsername", "azure_8d7499eb34262157d5f267e30b7bc214@azure.com");
-define("sendGridPassword", "lnalrka7");
+define("sendGridPassword", "xxxxxxxxx");
